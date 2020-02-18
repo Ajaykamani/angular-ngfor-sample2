17bdd03094fabcceb34ee5883b651d5a82708830
Ajaykamani <ajay.kamani3@gmail.com>
2020-02-19 00:07:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ngfor-sample2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ngfor-sample2)
---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+
+  books = [ 
+    {
+      title: 'Awesome Novel 1',
+      description: 'Short Description of awesome novel 1'
+    },
+    {
+      title: 'Awesome Novel 2',
+      description: 'Short Description of awesome novel 2'
+    },
+    {
+      title: 'Awesome Novel 3',
+      description: 'Short Description of awesome novel 3'
+    },
+    {
+      title: 'Awesome Novel 4',
+      description: 'Short Description of awesome novel 4'
+    },
+    {
+      title: 'Awesome Novel 5',
+      description: 'Short Description of awesome novel 5'
+    }
+  ];
+  
+}
